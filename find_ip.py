92b86a13965ca345885eda6a2d30eb0db90de1cd
Rosun GV <47551990+gvrosun@users.noreply.github.com>
2020-12-08 10:19:52 +0530

    v1.0

---
+++ find_ip.py
@@ -0,0 +1,33 @@
+from sys import argv
+from socket import gethostbyname, gaierror, getnameinfo, gethostbyaddr
+from os import system
+from termcolor import colored
+from re import search
+
+
+def get_ips(name):
+    ip_list = []
+    ips = ""
+    for i in range(0, 10):
+        ip_list.append(colored(gethostbyname(name), "green"))
+        ip_list = list(set(ip_list))
+    for ip in ip_list:
+        ips += ip + ", "
+    return ips[:-2]
+
+argv.pop(0)
+n = len(argv)
+system("clear -x")
+for domain_name in argv:
+    domain_name = domain_name.replace("https://", "")
+    domain_name = domain_name.replace("http://", "")
+    try:
+        search('[a-zA-Z]', domain_name).group(0)
+        print(domain_name, " --> ", get_ips(domain_name))
+    except gaierror:
+        print(domain_name, " --> ", colored("Invalid host", "red"))
+    except AttributeError:
+        try:
+            print(domain_name, " --> ", colored(getnameinfo((domain_name, 0), 0)[0], "green"))
+        except AttributeError:
+            print(domain_name, " --> ", colored("Invalid ip", "red"))
